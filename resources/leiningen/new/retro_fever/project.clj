9b3542c6e649de936a5ae6528c4e95a3669ed8a1
Jacob Emcken <je@casalogic.dk>
2015-01-04 00:49:20 +0100

    Fix template for project.clj

--- resources/leiningen/new/retro_fever/project.clj
+++ resources/leiningen/new/retro_fever/project.clj
@@ -1,6 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://github.com/jessitron/{{name}}"
+  :description "FIXME: write a game description"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
