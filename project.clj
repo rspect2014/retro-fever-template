5f1c82c0addf48de9d721ff38d819f59903b3c1e
Claus Engel-Christensen <cec@casalogic.dk>
2015-01-21 23:39:20 +0100

    Added new dependencies for the new template

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject retro-fever/lein-template "0.2.1"
+(defproject retro-fever/lein-template "0.3.0-SNAPSHOT"
   :description "Lein template for the retro-fever 2D game engine"
   :url "http://retro-fever.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/rspect2014/retro-fever-template.git"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :dependencies [[leinjacker "0.4.1"]
+                 [selmer "0.7.9"]])
